87633c03bdd4279d094110e19c5f20d66d209456
spiglerg <spiglerg@gmail.com>
2019-04-03 11:31:43 +0200

    placeholder for environments tests

---
+++ tests/test_environments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `flatland` package."""
+
+
+def test_base_environment():
+    """Test example Transition."""
+    a = True
+    b = True
+    assert a == b
