493b8a2027c83ce4683853a29bd9813a9be51eea
Guillaume Mollard <guillaume.mollard2@gmail.com>
2019-04-18 16:46:00 +0200

    started implementation of global view and the corresponding test

--- flatland/core/env_observation_builder.py
+++ flatland/core/env_observation_builder.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # TODO: add docstrings, pylint, etc...
 
 
@@ -23,3 +25,25 @@ class TreeObsForRailEnv(ObservationBuilder):
 
         # raise NotImplementedError()
         return []
+
+
+class GlobalObsForRailEnv(ObservationBuilder):
+    """
+    Gives a global observation of the entire rail environment.
+    The observation is composed of the following elements:
+
+        - transition map array with dimensions (env.height, env.width, 16),
+          assuming 16 bits encoding of transitions.
+
+        - Four 2D arrays containing respectively the position of the given agent,
+          the position of its target, the positions of the other agents and of
+          their target.
+    """
+    def __init__(self, env):
+        super(GlobalObsForRailEnv, self).__init__(env)
+        self.rail_obs = np.zeros((self.env.height, self.env.width, 16))
+        for i in range(self.rail_obs.shape[0]):
+            for j in range(self.rail_obs.shape[1]):
+                self.rail_obs[i, j] = self.env.rail.get_transitions((i, j))
+
+
