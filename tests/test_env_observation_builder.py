ae4c9178e3fba080cb71e64808204defd5b18146
Guillaume Mollard <guillaume.mollard2@gmail.com>
2019-05-24 15:13:32 +0200

    fixed test fail for Global Obs

--- tests/test_env_observation_builder.py
+++ tests/test_env_observation_builder.py
@@ -82,7 +82,7 @@ def test_global_obs():
 
     # If this assertion is wrong, it means that the observation returned
     # places the agent on an empty cell
-    assert(np.sum(rail_map * global_obs[0][1][0]) > 0)
+    assert(np.sum(rail_map * global_obs[0][1][:, :, 0]) > 0)
 
 
 def main():
