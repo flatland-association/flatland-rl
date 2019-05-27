39c57a263b3f9f6e946618776e5d965e2870ced1
hagrid67 <jdhwatson@gmail.com>
2019-05-27 17:05:33 +0100

    fix lint errors from merge

--- flatland/utils/graphics_pil.py
+++ flatland/utils/graphics_pil.py
@@ -1,4 +1,3 @@
-
 from flatland.utils.graphics_layer import GraphicsLayer
 from PIL import Image, ImageDraw, ImageTk   # , ImageFont
 import tkinter as tk
@@ -37,11 +36,8 @@ class PILGL(GraphicsLayer):
         self.window_open = False
         # self.bShow = show
         self.firstFrame = True
-<<<<<<< HEAD
         self.create_layers()
-=======
->>>>>>> 8062e72b7eec9bab1e71f4c0beb4d9ec9cd67026
-        self.beginFrame()
+        # self.beginFrame()
 
     def plot(self, gX, gY, color=None, linewidth=3, layer=0, opacity=255, **kwargs):
         color = self.adaptColor(color)
@@ -60,7 +56,6 @@ class PILGL(GraphicsLayer):
         for x, y in gPoints:
             self.draws[layer].rectangle([(x - r, y - r), (x + r, y + r)], fill=color, outline=color)
 
-<<<<<<< HEAD
     def drawImageXY(self, pil_img, xyPixLeftTop, layer=0):
         # self.layers[layer].alpha_composite(pil_img, offset=xyPixLeftTop)
         if (pil_img.mode == "RGBA"): 
@@ -75,8 +70,6 @@ class PILGL(GraphicsLayer):
         xyPixLeftTop = tuple((array(rcTopLeft) * self.nPixCell)[[1, 0]])
         self.drawImageXY(pil_img, xyPixLeftTop, layer=layer)
 
-=======
->>>>>>> 8062e72b7eec9bab1e71f4c0beb4d9ec9cd67026
     def open_window(self):
         assert self.window_open is False, "Window is already open!"
         self.window = tk.Tk()
@@ -106,10 +99,7 @@ class PILGL(GraphicsLayer):
         tkimg = ImageTk.PhotoImage(img)
         
         if self.firstFrame:
-<<<<<<< HEAD
             # Do TK actions for a new panel (not sure what they really do)
-=======
->>>>>>> 8062e72b7eec9bab1e71f4c0beb4d9ec9cd67026
             self.panel = tk.Label(self.window, image=tkimg)
             self.panel.pack(side="bottom", fill="both", expand="yes")
         else:
