1a4664cf0c338e52b74e8fff4594819c287ba723
Egli Adrian (IT-SCI-API-PFI) <adrian.egli@sbb.ch>
2019-06-13 12:10:31 +0200

    .

--- flatland/utils/graphics_pil.py
+++ flatland/utils/graphics_pil.py
@@ -433,7 +433,7 @@ class PILSVG(PILGL):
                             pilTrack = self.dScenery[0]
                         else:
                             pilTrack = self.dBuildings[a]
-                    elif self.background_grid[col][row] > 5:
+                    elif self.background_grid[col][row] > 5 or ((col + row + col * row) % 10 > 7):
                         a = int(self.background_grid[col][row]) - 5
                         a = a % len(self.dScenery)
                         if (col + row + col * row) % 10 > 2:
