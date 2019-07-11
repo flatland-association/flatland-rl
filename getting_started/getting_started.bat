bcb637494d8da54d841c38c33300957d74b3dcfa
u214892 <u214892@sbb.ch>
2019-07-11 11:46:01 +0200

    #97 tox-conda #94 use miniconda instead of anaconda

--- getting_started/getting_started.bat
+++ getting_started/getting_started.bat
@@ -4,9 +4,9 @@ set FLATLAND_BASEDIR=%~dp0\..
 
 cd %FLATLAND_BASEDIR%
 
-conda install -c conda-forge tox-conda || goto :error
-conda install tox || goto :error
-tox -v -e start_jupyter --recreate || goto :error
+call conda install -y -c conda-forge tox-conda || goto :error
+call conda install -y tox || goto :error
+call tox -v -e start_jupyter --recreate || goto :error
 
 
 goto :EOF
--- getting_started/getting_started.sh
+++ getting_started/getting_started.sh
@@ -8,6 +8,6 @@ FLATLAND_BASEDIR=$(dirname "$0")/..
 FLATLAND_BASEDIR=$(realpath "$FLATLAND_BASEDIR")
 cd ${FLATLAND_BASEDIR}
 
-conda install -c conda-forge tox-conda
-conda install tox
+conda install -y -c conda-forge tox-conda
+conda install -y tox
 tox -v -e start_jupyter &
