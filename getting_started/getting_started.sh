7c740e61427b6f1612c21d61ad30c3b0819f6072
u214892 <u214892@sbb.ch>
2019-07-11 14:58:56 +0200

    #97 tox-conda #94 use miniconda instead of anaconda

--- getting_started/getting_started.sh
+++ getting_started/getting_started.sh
@@ -3,9 +3,7 @@ set -e # stop on error
 set -x # echo commands
 
 
-# https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 FLATLAND_BASEDIR=$(dirname "$0")/..
-FLATLAND_BASEDIR=$(realpath "$FLATLAND_BASEDIR")
 cd ${FLATLAND_BASEDIR}
 
 conda install -y -c conda-forge tox-conda
