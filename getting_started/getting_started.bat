c0adf2de189cf4df07bdeefe2cb0237e94008280
u214892 <u214892@sbb.ch>
2019-07-12 14:04:41 +0200

    #97 tox-conda #94 use miniconda instead of anaconda

--- getting_started/getting_started.bat
+++ getting_started/getting_started.bat
@@ -6,8 +6,8 @@ cd %FLATLAND_BASEDIR%
 
 call conda install -y -c conda-forge tox-conda || goto :error
 call conda install -y tox || goto :error
+call tox -v  --recreate || goto :error
 call tox -v -e start_jupyter --recreate || goto :error
-rem call tox -v -e start_jupyter || goto :error
 
 goto :EOF
 
--- getting_started/getting_started.sh
+++ getting_started/getting_started.sh
@@ -8,4 +8,5 @@ cd ${FLATLAND_BASEDIR}
 
 conda install -y -c conda-forge tox-conda
 conda install -y tox
+tox -v
 tox -v -e start_jupyter &
