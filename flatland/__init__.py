96feebff3475f0549838f24c4dbd00a25ee6c9be
SP Mohanty <spmohanty91@gmail.com>
2019-07-30 03:07:12 +0200

    Bump version: 0.3.7 → 0.3.8

--- flatland/__init__.py
+++ flatland/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """S.P. Mohanty"""
 __email__ = 'mohanty@aicrowd.com'
-__version__ = '0.3.7'
+__version__ = '0.3.8'
--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.aicrowd.com/flatland/flatland',
-    version='0.3.7',
+    version='0.3.8',
     zip_safe=False,
 )
