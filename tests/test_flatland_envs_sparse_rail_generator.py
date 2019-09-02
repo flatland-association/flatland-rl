ea7a351eb41a28c0d96fa69ee1c3b6f4cc123781
u214892 <u214892@sbb.ch>
2019-09-02 17:03:19 +0200

    #154 actionable agents in info dict

--- tests/test_flatland_envs_sparse_rail_generator.py
+++ tests/test_flatland_envs_sparse_rail_generator.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from flatland.envs.observations import GlobalObsForRailEnv
 from flatland.envs.rail_env import RailEnv
 from flatland.envs.rail_generators import sparse_rail_generator
@@ -24,3 +26,83 @@ def test_sparse_rail_generator():
     env_renderer = RenderTool(env, gl="PILSVG", )
     env_renderer.render_env(show=True, show_observations=True, show_predictions=False)
     env_renderer.gl.save_image("./sparse_generator_false.png")
+    # TODO test assertions!
+
+
+def test_rail_env_actionable():
+    np.random.seed(0)
+    speed_ration_map = {1.: 0.25,  # Fast passenger train
+                        1. / 2.: 0.25,  # Fast freight train
+                        1. / 3.: 0.25,  # Slow commuter train
+                        1. / 4.: 0.25}  # Slow freight train
+    env_always_action = RailEnv(width=50,
+                                height=50,
+                                rail_generator=sparse_rail_generator(num_cities=10,  # Number of cities in map
+                                                                     num_intersections=10,
+                                                                     # Number of interesections in map
+                                                                     num_trainstations=50,
+                                                                     # Number of possible start/targets on map
+                                                                     min_node_dist=6,  # Minimal distance of nodes
+                                                                     node_radius=3,
+                                                                     # Proximity of stations to city center
+                                                                     num_neighb=3,
+                                                                     # Number of connections to other cities
+                                                                     seed=5,  # Random seed
+                                                                     grid_mode=False  # Ordered distribution of nodes
+                                                                     ),
+                                schedule_generator=sparse_schedule_generator(speed_ration_map),
+                                number_of_agents=10,
+                                obs_builder_object=GlobalObsForRailEnv())
+    np.random.seed(0)
+    env_only_if_actionable = RailEnv(width=50,
+                                     height=50,
+                                     rail_generator=sparse_rail_generator(num_cities=10,  # Number of cities in map
+                                                                          num_intersections=10,
+                                                                          # Number of interesections in map
+                                                                          num_trainstations=50,
+                                                                          # Number of possible start/targets on map
+                                                                          min_node_dist=6,  # Minimal distance of nodes
+                                                                          node_radius=3,
+                                                                          # Proximity of stations to city center
+                                                                          num_neighb=3,
+                                                                          # Number of connections to other cities
+                                                                          seed=5,  # Random seed
+                                                                          grid_mode=False
+                                                                          # Ordered distribution of nodes
+                                                                          ),
+                                     schedule_generator=sparse_schedule_generator(speed_ration_map),
+                                     number_of_agents=10,
+                                     obs_builder_object=GlobalObsForRailEnv())
+    env_renderer = RenderTool(env_always_action, gl="PILSVG", )
+
+    for step in range(100):
+        print("step {}".format(step))
+
+        action_dict_always_action = dict()
+        action_dict_only_if_actionable = dict()
+        # Chose an action for each agent in the environment
+        for a in range(env_always_action.get_num_agents()):
+            action = np.random.choice(np.arange(4))
+            action_dict_always_action.update({a: action})
+            if step == 0 or info_only_if_actionable['actionable_agents'][a]:
+                action_dict_only_if_actionable.update({a: action})
+            else:
+                print("[{}] not actionable {}, speed_data={}".format(step, a, env_always_action.agents[a].speed_data))
+
+        obs_always_action, rewards_always_action, done_always_action, info_always_action = env_always_action.step(
+            action_dict_always_action)
+        obs_only_if_actionable, rewards_only_if_actionable, done_only_if_actionable, info_only_if_actionable = env_only_if_actionable.step(
+            action_dict_only_if_actionable)
+
+        for a in range(env_always_action.get_num_agents()):
+            assert len(obs_always_action[a]) == len(obs_only_if_actionable[a])
+            for i in range(len(obs_always_action[a])):
+                assert np.array_equal(obs_always_action[a][i], obs_only_if_actionable[a][i])
+            assert np.array_equal(rewards_always_action[a], rewards_only_if_actionable[a])
+            assert np.array_equal(done_always_action[a], done_only_if_actionable[a])
+            assert info_always_action['actionable_agents'][a] == info_only_if_actionable['actionable_agents'][a]
+
+        env_renderer.render_env(show=True, show_observations=False, show_predictions=False)
+
+        if done_always_action['__all__']:
+            break
