38857345c8634b4db1eee96a55c6ca2504e939e6
SP Mohanty <spmohanty91@gmail.com>
2019-09-06 15:00:48 +0200

    Bump version: 0.3.10 → 1.0.0

--- setup.py
+++ setup.py
@@ -79,6 +79,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.aicrowd.com/flatland/flatland',
-    version='0.3.10',
+    version='1.0.0',
     zip_safe=False,
 )
