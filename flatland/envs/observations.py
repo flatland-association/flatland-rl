7ecdafb9c8dbf9ba8bc2e67727e1d2315c289d0e
MLErik <baerenjesus@gmail.com>
2019-10-02 18:48:35 -0400

    added possibility of multiple agents on same location to tree observation

--- flatland/envs/observations.py
+++ flatland/envs/observations.py
@@ -160,7 +160,12 @@ class TreeObsForRailEnv(ObservationBuilder):
         """
 
         # Update local lookup table for all agents' positions
-        self.location_has_agent = {tuple(agent.position): 1 for agent in self.env.agents}
+        self.location_has_agent = dict()
+        for agent in self.env.agents:
+            if tuple(agent.position) in self.location_has_agent:
+                self.location_has_agent[tuple(agent.position)] = self.location_has_agent[tuple(agent.position)] + 1
+            else:
+                self.location_has_agent[tuple(agent.position)] = 1
         # TODO: Update this to handle number of agents at same location
         self.location_has_agent_direction = {tuple(agent.position): agent.direction for agent in self.env.agents}
         self.location_has_agent_speed = {tuple(agent.position): agent.speed_data['speed'] for agent in self.env.agents}
