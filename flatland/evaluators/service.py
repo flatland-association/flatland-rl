7daa31b85e566d44c17c8248394b87f213db0c74
S.P. Mohanty <spmohanty91@gmail.com>
2019-10-23 17:45:02 +0200

    Use RailAgentStatus when computing the percentage of done agents

--- flatland/evaluators/service.py
+++ flatland/evaluators/service.py
@@ -18,6 +18,7 @@ import timeout_decorator
 
 from flatland.core.env_observation_builder import DummyObservationBuilder
 from flatland.envs.rail_env import RailEnv
+from flatland.envs.agent_utils import RailAgentStatus
 from flatland.envs.rail_generators import rail_from_file
 from flatland.envs.schedule_generators import schedule_from_file
 from flatland.evaluators import aicrowd_helpers
@@ -445,7 +446,7 @@ class FlatlandRemoteEvaluationService:
             complete = 0
             for i_agent in range(self.env.get_num_agents()):
                 agent = self.env.agents[i_agent]
-                if agent.position == agent.target:
+                if agent.status in [RailAgentStatus.DONE, RailAgentStatus.DONE_REMOVED]:
                     complete += 1
             percentage_complete = complete * 1.0 / self.env.get_num_agents()
             self.simulation_percentage_complete[-1] = percentage_complete
