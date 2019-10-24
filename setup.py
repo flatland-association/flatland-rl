9ee8a299c31a4ac540261e5053968f1e29e043c4
Shivam Khandelwal <shivam@aicrowd.com>
2019-10-24 19:54:12 +0000

    Bump version: 2.1.7 → 2.1.8

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.aicrowd.com/flatland/flatland',
-    version='2.1.7',
+    version='2.1.8',
     zip_safe=False,
 )
