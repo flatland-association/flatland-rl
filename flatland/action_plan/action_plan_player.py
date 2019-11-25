33701dd2d91965a93f5616147a9b885ee29dcb07
u214892 <u214892@sbb.ch>
2019-11-25 10:39:54 -0500

    make FLATland core useable without rendering

--- flatland/action_plan/action_plan_player.py
+++ flatland/action_plan/action_plan_player.py
@@ -1,23 +1,26 @@
+from typing import Callable
+
 from flatland.action_plan.action_plan import ControllerFromTrainruns
 from flatland.envs.rail_env import RailEnv
 from flatland.envs.rail_trainrun_data_structures import Waypoint
-from flatland.utils.rendertools import RenderTool, AgentRenderVariant
 
 
 class ControllerFromTrainrunsReplayer():
     """Allows to verify a `DeterministicController` by replaying it against a FLATland env without malfunction."""
 
     @staticmethod
-    def replay_verify(ctl: ControllerFromTrainruns, env: RailEnv, rendering: bool):
-        """Replays this deterministic `ActionPlan` and verifies whether it is feasible."""
-        if rendering:
-            renderer = RenderTool(env, gl="PILSVG",
-                                  agent_render_variant=AgentRenderVariant.AGENT_SHOWS_OPTIONS_AND_BOX,
-                                  show_debug=True,
-                                  clear_debug_text=True,
-                                  screen_height=1000,
-                                  screen_width=1000)
-            renderer.render_env(show=True, show_observations=False, show_predictions=False)
+    def replay_verify(ctl: ControllerFromTrainruns, env: RailEnv,
+                      call_back: Callable[[RailEnv], None] = lambda *a, **k: None):
+        """Replays this deterministic `ActionPlan` and verifies whether it is feasible.
+
+        Parameters
+        ----------
+        ctl
+        env
+        call_back
+            Called before/after each step() call. The env is passed to it.
+        """
+        call_back(env)
         i = 0
         while not env.dones['__all__'] and i <= env._max_episode_steps:
             for agent_id, agent in enumerate(env.agents):
@@ -30,7 +33,6 @@ class ControllerFromTrainrunsReplayer():
 
             obs, all_rewards, done, _ = env.step(actions)
 
-            if rendering:
-                renderer.render_env(show=True, show_observations=False, show_predictions=False)
+            call_back(env)
 
             i += 1
