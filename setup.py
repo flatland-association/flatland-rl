e930b20b5f2a5a347d227f1162a71d9e644b9b56
flaurent <florian.laurent@gmail.com>
2020-06-12 03:19:39 +0200

    Bump version: 2.2.0 → 2.2.1

--- setup.py
+++ setup.py
@@ -80,6 +80,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.aicrowd.com/flatland/flatland',
-    version='2.2.0',
+    version='2.2.1',
     zip_safe=False,
 )
