73dd5f65a8ce78bd1c9d800bb9003bb351d3fc13
nimishsantosh107 <nimishsantosh107@icloud.com>
2021-09-02 11:32:13 +0530

    cli.py fixed

--- flatland/cli.py
+++ flatland/cli.py
@@ -9,8 +9,8 @@ import numpy as np
 import redis
 
 from flatland.envs.rail_env import RailEnv
-from flatland.envs.rail_generators import complex_rail_generator
-from flatland.envs.line_generators import complex_line_generator
+from flatland.envs.rail_generators import sparse_rail_generator
+from flatland.envs.line_generators import sparse_line_generator
 from flatland.evaluators.service import FlatlandRemoteEvaluationService
 from flatland.utils.rendertools import RenderTool
 
@@ -18,35 +18,42 @@ from flatland.utils.rendertools import RenderTool
 @click.command()
 def demo(args=None):
     """Demo script to check installation"""
-    env = RailEnv(width=15, height=15, rail_generator=complex_rail_generator(
-        nr_start_goal=10,
-        nr_extra=1,
-        min_dist=8,
-        max_dist=99999), line_generator=complex_line_generator(), number_of_agents=5)
+    env = RailEnv(
+        width=30, 
+        height=30, 
+        rail_generator=sparse_rail_generator(
+            max_num_cities=3,
+            grid_mode=False,
+            max_rails_between_cities=4,
+            max_rail_pairs_in_city=2,
+            seed=0
+        ),
+        line_generator=sparse_line_generator(), 
+        number_of_agents=5)
 
     env._max_episode_steps = int(15 * (env.width + env.height))
     env_renderer = RenderTool(env)
 
-    while True:
-        obs, info = env.reset()
-        _done = False
-        # Run a single episode here
-        step = 0
-        while not _done:
-            # Compute Action
-            _action = {}
-            for _idx, _ in enumerate(env.agents):
-                _action[_idx] = np.random.randint(0, 5)
-            obs, all_rewards, done, _ = env.step(_action)
-            _done = done['__all__']
-            step += 1
-            env_renderer.render_env(
-                show=True,
-                frames=False,
-                show_observations=False,
-                show_predictions=False
-            )
-            time.sleep(0.3)
+    obs, info = env.reset()
+    _done = False
+    # Run a single episode here
+    step = 0
+    while not _done:
+        # Compute Action
+        _action = {}
+        for _idx, _ in enumerate(env.agents):
+            _action[_idx] = np.random.randint(0, 5)
+        obs, all_rewards, done, _ = env.step(_action)
+        _done = done['__all__']
+        step += 1
+        env_renderer.render_env(
+            show=True,
+            frames=False,
+            show_observations=False,
+            show_predictions=False
+        )
+        time.sleep(0.1)
+        
     return 0
 
 
