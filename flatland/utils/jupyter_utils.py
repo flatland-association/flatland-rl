62bc440634ccfcd46303f02ce67eccad66a2f7b7
adrian_egli <adrian.egli@gmail.com>
2022-07-11 17:42:33 +0200

    Jupyter notebooks are ready for flatland3. But they should be overworked. Might just one example per use case would be much more clear. TODO - cleanup

--- flatland/utils/jupyter_utils.py
+++ flatland/utils/jupyter_utils.py
@@ -2,6 +2,7 @@ from typing import List, NamedTuple
 
 from IPython import display
 from ipycanvas import canvas
+
 from flatland.envs.rail_env import RailEnvActions
 from flatland.utils.rendertools import RenderTool
 
@@ -110,7 +111,7 @@ class EnvCanvas():
 
     def render(self):
         self.oRT.render_env(show_rowcols=True, show_inactive_agents=False, show_observations=False)
-        self.oCan.put_image_data(self.oRT.get_image()[:,:,0:3])
+        self.oCan.put_image_data(self.oRT.get_image()[:, :, 0:3])
 
     def step(self):
         dAction = self.behaviour.getActions()
