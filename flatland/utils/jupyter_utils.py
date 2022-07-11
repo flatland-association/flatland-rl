b044ae56de41a195a9c08b03d93b5144151ee27c
adrian_egli <adrian.egli@gmail.com>
2022-07-11 14:19:04 +0200

    Jupyter notebooks are ready for flatland3. But they should be overworked. Might just one example per use case would be much more clear. TODO - cleanup

--- flatland/utils/jupyter_utils.py
+++ flatland/utils/jupyter_utils.py
@@ -1,21 +1,10 @@
+from typing import List, NamedTuple
 
-
-import PIL
 from IPython import display
 from ipycanvas import canvas
-import time
-
-from flatland.envs import malfunction_generators as malgen
-from flatland.envs.agent_utils import EnvAgent
-#from flatland.envs import sparse_rail_gen as spgen
-from flatland.envs import rail_generators as rail_gen
-from flatland.envs import agent_chains as ac
-from flatland.envs.rail_env import RailEnv, RailEnvActions
-
-from flatland.envs.persistence import RailEnvPersister
+from flatland.envs.rail_env import RailEnvActions
 from flatland.utils.rendertools import RenderTool
-from flatland.utils import env_edit_utils as eeu
-from typing import List, NamedTuple
+
 
 class Behaviour():
     def __init__(self, env):
@@ -24,10 +13,12 @@ class Behaviour():
 
     def getActions(self):
         return {}
-    
+
+
 class AlwaysForward(Behaviour):
     def getActions(self):
-        return { i:RailEnvActions.MOVE_FORWARD for i in range(self.nAg) }
+        return {i: RailEnvActions.MOVE_FORWARD for i in range(self.nAg)}
+
 
 class DelayedStartForward(AlwaysForward):
     def __init__(self, env, nStartDelay=2):
@@ -37,17 +28,19 @@ class DelayedStartForward(AlwaysForward):
     def getActions(self):
         iStep = self.env._elapsed_steps + 1
         nAgentsMoving = min(self.nAg, iStep // self.nStartDelay)
-        return { i:RailEnvActions.MOVE_FORWARD for i in range(nAgentsMoving) }
+        return {i: RailEnvActions.MOVE_FORWARD for i in range(nAgentsMoving)}
 
-AgentPause = NamedTuple("AgentPause", 
-    [
-        ("iAg", int),
-        ("iPauseAt", int),
-        ("iPauseFor", int)
-    ])
+
+AgentPause = NamedTuple("AgentPause",
+                        [
+                            ("iAg", int),
+                            ("iPauseAt", int),
+                            ("iPauseFor", int)
+                        ])
+
 
 class ForwardWithPause(Behaviour):
-    def __init__(self, env, lPauses:List[AgentPause]):
+    def __init__(self, env, lPauses: List[AgentPause]):
         self.env = env
         self.nAg = len(env.agents)
         self.lPauses = lPauses
@@ -57,39 +50,40 @@ class ForwardWithPause(Behaviour):
         iStep = self.env._elapsed_steps + 1  # add one because this is called before step()
 
         # new pauses starting this step
-        lNewPauses = [ tPause for tPause in self.lPauses if tPause.iPauseAt == iStep ]
+        lNewPauses = [tPause for tPause in self.lPauses if tPause.iPauseAt == iStep]
 
         # copy across the agent index and pause length
         for pause in lNewPauses:
             self.dAgPaused[pause.iAg] = pause.iPauseFor
 
         # default action is move forward
-        dAction = { i:RailEnvActions.MOVE_FORWARD for i in range(self.nAg) }
+        dAction = {i: RailEnvActions.MOVE_FORWARD for i in range(self.nAg)}
 
         # overwrite paused agents with stop
         for iAg in self.dAgPaused:
             dAction[iAg] = RailEnvActions.STOP_MOVING
-        
+
         # decrement the counters for each pause, and remove any expired pauses.
         lFinished = []
         for iAg in self.dAgPaused:
             self.dAgPaused[iAg] -= 1
             if self.dAgPaused[iAg] <= 0:
                 lFinished.append(iAg)
-        
+
         for iAg in lFinished:
             self.dAgPaused.pop(iAg, None)
-        
+
         return dAction
+
 
 class Deterministic(Behaviour):
     def __init__(self, env, dAg_lActions):
         super().__init__(env)
         self.dAg_lActions = dAg_lActions
-    
+
     def getActions(self):
         iStep = self.env._elapsed_steps
-        
+
         dAg_Action = {}
         for iAg, lActions in self.dAg_lActions.items():
             if iStep < len(lActions):
@@ -97,16 +91,13 @@ class Deterministic(Behaviour):
             else:
                 iAct = RailEnvActions.DO_NOTHING
             dAg_Action[iAg] = iAct
-        #print(iStep, dAg_Action[0])
+        # print(iStep, dAg_Action[0])
         return dAg_Action
-
-
-
 
 
 class EnvCanvas():
 
-    def __init__(self, env, behaviour:Behaviour=None):
+    def __init__(self, env, behaviour: Behaviour = None):
         self.env = env
         self.iStep = 0
         if behaviour is None:
@@ -114,11 +105,11 @@ class EnvCanvas():
         self.behaviour = behaviour
         self.oRT = RenderTool(env, show_debug=True)
 
-        self.oCan = canvas.Canvas(size=(600,300))
+        self.oCan = canvas.Canvas(size=(600, 300))
         self.render()
 
     def render(self):
-        self.oRT.render_env(show_rowcols=True,  show_inactive_agents=False, show_observations=False)
+        self.oRT.render_env(show_rowcols=True, show_inactive_agents=False, show_observations=False)
         self.oCan.put_image_data(self.oRT.get_image())
 
     def step(self):
@@ -128,5 +119,3 @@ class EnvCanvas():
     def show(self):
         self.render()
         display.display(self.oCan)
-
-
