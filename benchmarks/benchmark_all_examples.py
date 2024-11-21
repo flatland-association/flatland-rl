2d7f1e7bf56713a251ed714d20203f812e956fd2
chenkins <chenkins44@gmail.com>
2024-11-21 08:55:04 +0100

    Try debugging all examples.

--- benchmarks/benchmark_all_examples.py
+++ benchmarks/benchmark_all_examples.py
@@ -1,10 +1,16 @@
 import os
+import runpy
+import sys
+import traceback
+from io import StringIO
 from time import sleep
 
 import importlib_resources
 import pkg_resources
 from benchmarker import Benchmarker
 from importlib_resources import path
+
+from benchmarks.benchmark_utils import swap_attr
 
 
 def benchmark_all_examples():
@@ -23,27 +29,30 @@ def benchmark_all_examples():
         if benchmarks_output_folder:
             outfile = os.path.join(benchmarks_output_folder, f"{entry}.json")
         print(f"outfile={outfile}")
+        try:
+            with path('examples', entry) as file_in:
 
-        with path('examples', entry) as file_in:
+                with Benchmarker(cycle=20, extra=1, outfile=outfile) as bench:
+                    @bench(entry)
+                    def _(_):
+                        # prevent Benchmarker from doing "ZeroDivisionError: float division by zero:
+                        #    ratio = base_time / real_time"
+                        sleep(0.001)
+                        # In order to pipe input into examples that have input(),
+                        # we use the test package, which is meant for internal use by Python only internal and
+                        # Any use of this package outside of Python’s standard library is discouraged as code (..)
+                        # can change or be removed without notice between releases of Python.
+                        # https://docs.python.org/3/library/test.html
+                        # TODO remove input() from examples?
+                        with swap_attr(sys, "stdin", StringIO("q")):
+                            runpy.run_path(file_in, run_name="__main__", init_globals={
+                                'argv': ['--sleep-for-animation=False', '--do_rendering=False']
+                            })
 
-            with Benchmarker(cycle=20, extra=1, outfile=outfile) as bench:
-                @bench(entry)
-                def _(_):
-                    # prevent Benchmarker from doing "ZeroDivisionError: float division by zero:
-                    #    ratio = base_time / real_time"
-                    sleep(0.001)
-                    # In order to pipe input into examples that have input(),
-                    # we use the test package, which is meant for internal use by Python only internal and
-                    # Any use of this package outside of Python’s standard library is discouraged as code (..)
-                    # can change or be removed without notice between releases of Python.
-                    # https://docs.python.org/3/library/test.html
-                    with open(outfile, "w") as o:
-                        o.write("abcd")
-        #             # TODO remove input() from examples?
-        #             with swap_attr(sys, "stdin", StringIO("q")):
-        #                 runpy.run_path(file_in, run_name="__main__", init_globals={
-        #                     'argv': ['--sleep-for-animation=False', '--do_rendering=False']
-        #                 })
+        except:
+            print("runpy failed:")
+            traceback.print_exc()
+        print("runpy done.")
 
 
 if __name__ == '__main__':
