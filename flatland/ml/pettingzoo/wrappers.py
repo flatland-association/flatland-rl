ca4e29c07381c0bbb63c9dc2dbf5a3095ef04045
chenkins <chenkins44@gmail.com>
2024-12-15 21:51:59 +0100

    Fix Pettingzoo eval with parallel env.

--- flatland/ml/pettingzoo/wrappers.py
+++ flatland/ml/pettingzoo/wrappers.py
@@ -2,12 +2,12 @@ from envs.rail_env import RailEnv
 from ml.pettingzoo.pettingzoo_parallel_rail_env import PettingZooParallelEnvWrapper
 
 
-class PettingzooFlatland():
+class PettingzooFlatland:
     def __init__(self, wrap: RailEnv):
         assert hasattr(wrap.obs_builder, "get_observation_space"), f"{type(wrap.obs_builder)} is not gym-compatible, missing get_observation_space"
         self.wrap = wrap
 
-    # TODO should be AEC instead
+    # TODO should be AEC instead?
     def env(self, **kwargs):
         return PettingZooParallelEnvWrapper(self.wrap, **kwargs)
 
