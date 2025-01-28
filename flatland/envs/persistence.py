c7fced594802acc815be61faf0c5ea844b5aae19
chenkins <chenkins44@gmail.com>
2025-01-28 22:33:57 +0100

    Fix circular import

--- flatland/envs/persistence.py
+++ flatland/envs/persistence.py
@@ -99,7 +99,7 @@ class RailEnvPersister(object):
         cls.set_full_state(env, env_dict)
 
     @classmethod
-    def load_new(cls, filename, load_from_package=None) -> Tuple[rail_env.RailEnv, Dict]:
+    def load_new(cls, filename, load_from_package=None) -> Tuple["RailEnv", Dict]:
 
         env_dict = cls.load_env_dict(filename, load_from_package=load_from_package)
 
