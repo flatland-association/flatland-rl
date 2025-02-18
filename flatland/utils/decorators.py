f12082b5dc2aa7ff71c8cc5c9970f1a92fa5adbd
chenkins <chenkins44@gmail.com>
2025-02-18 17:11:28 +0100

    Get rid of methodtools and go back to functools.lru_cache using proper eq/hash definition on instance methods.

--- flatland/utils/decorators.py
+++ flatland/utils/decorators.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 infrastructure_lru_cache_functions = []
 
 
-# TODO https://github.com/flatland-association/flatland-rl/issues/104 1. revise which caches need to be scoped at all - some seem not to require cache clearing at all. 2. refactor with need to explicitly reset cache in calls dispersed in the whole code base. Use classes to group the cache scope using methodtools for instance method lru caching.
+# TODO https://github.com/flatland-association/flatland-rl/issues/104 1. revise which caches need to be scoped at all - some seem not to require cache clearing at all. 2. refactor with need to explicitly reset cache in calls dispersed in the whole code base. Use classes to group the cache scope by overriding eq/hash for instance method lru caching (see https://docs.python.org/3/faq/programming.html#how-do-i-cache-method-calls)
 def enable_infrastructure_lru_cache(*args, **kwargs):
     def decorator(func):
         func = lru_cache(*args, **kwargs)(func)
@@ -13,9 +13,8 @@ def enable_infrastructure_lru_cache(*args, **kwargs):
     return decorator
 
 
-# send_infrastructure_data_change_signal_to_reset_lru_cache() has a problem with instance methods - the methods are not properly cleared.
-# Therefore, make sure to use methodtools for instance methods and to instantiantiate new objects to match instance and cache lifecycle.
-# See https://stackoverflow.com/questions/33672412/python-functools-lru-cache-with-instance-methods-release-object
+# send_infrastructure_data_change_signal_to_reset_lru_cache() has a problem with instance methods - the methods are not properly cleared by it.
+# Therefore, make sure to override eq/hash to control cache lifecycle for instance method lru caching (see https://stackoverflow.com/questions/33672412/python-functools-lru-cache-with-instance-methods-release-object and https://docs.python.org/3/faq/programming.html#how-do-i-cache-method-calls)
 def send_infrastructure_data_change_signal_to_reset_lru_cache():
     for func in infrastructure_lru_cache_functions:
         func.cache_clear()
