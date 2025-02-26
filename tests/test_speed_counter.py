c0f37c68b3a195d71127c6f429b7b03725a8931b
chenkins <chenkins44@gmail.com>
2025-02-26 16:04:39 +0100

    Add test speed counter.

---
+++ tests/test_speed_counter.py
@@ -0,0 +1,55 @@
+from flatland.envs.step_utils.speed_counter import SpeedCounter
+from flatland.envs.step_utils.states import TrainState
+
+
+def test_update_counter_speed025():
+    sc = SpeedCounter(speed=0.25)
+
+    assert sc.is_cell_entry == True
+    assert sc.is_cell_exit == False
+    assert sc.counter == 0
+    assert sc.max_count == 3
+
+    sc.update_counter(TrainState.MOVING, (0, 0))
+    assert sc.is_cell_entry == False
+    assert sc.is_cell_exit == False
+    assert sc.counter == 1
+    assert sc.max_count == 3
+
+    sc.update_counter(TrainState.MOVING, (0, 0))
+    assert sc.is_cell_entry == False
+    assert sc.is_cell_exit == False
+    assert sc.counter == 2
+    assert sc.max_count == 3
+
+    sc.update_counter(TrainState.MOVING, (0, 0))
+    assert sc.is_cell_entry == False
+    assert sc.is_cell_exit == True
+    assert sc.counter == 3
+    assert sc.max_count == 3
+
+    sc.update_counter(TrainState.MOVING, (1, 0))
+    assert sc.is_cell_entry == True
+    assert sc.is_cell_exit == False
+    assert sc.counter == 0
+    assert sc.max_count == 3
+
+
+def test_update_counter_speed05():
+    sc = SpeedCounter(speed=0.5)
+    assert sc.is_cell_entry == True
+    assert sc.is_cell_exit == False
+    assert sc.counter == 0
+    assert sc.max_count == 1
+
+    sc.update_counter(TrainState.MOVING, (0, 0))
+    assert sc.is_cell_entry == False
+    assert sc.is_cell_exit == True
+    assert sc.counter == 1
+    assert sc.max_count == 1
+
+    sc.update_counter(TrainState.MOVING, (0, 0))
+    assert sc.is_cell_entry == True
+    assert sc.is_cell_exit == False
+    assert sc.counter == 0
+    assert sc.max_count == 1
