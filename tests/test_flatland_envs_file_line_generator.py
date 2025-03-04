c1788065f3df505bdeddc1d0c7a4937266aeee15
chenkins <chenkins44@gmail.com>
2025-03-04 16:00:56 +0100

    Fix relative import.

--- tests/test_flatland_envs_file_line_generator.py
+++ tests/test_flatland_envs_file_line_generator.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import numpy as np
 
-from envs.line_generators import FileLineGenerator
+from flatland.envs.line_generators import FileLineGenerator
 from flatland.envs.timetable_utils import Line
 
 
