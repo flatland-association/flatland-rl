027361334c206815bffaff505f3fe67955776bfe
chenkins <chenkins44@gmail.com>
2025-03-05 11:00:17 +0100

    Add timetable_from_file. Update service test pkls after reset (to contain state after reset).
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- flatland/envs/timetable_generators.py
+++ flatland/envs/timetable_generators.py
@@ -1,16 +1,16 @@
-import os
-import json
-import itertools
 import warnings
-from typing import Tuple, List, Callable, Mapping, Optional, Any
-from flatland.envs.timetable_utils import Timetable
+from pathlib import Path
+from typing import Tuple, List
 
 import numpy as np
 from numpy.random.mtrand import RandomState
 
+from flatland.envs import persistence
 from flatland.envs.agent_utils import EnvAgent
 from flatland.envs.distance_map import DistanceMap
 from flatland.envs.rail_env_shortest_paths import get_shortest_paths
+from flatland.envs.timetable_utils import Timetable
+
 
 def len_handle_none(v):
     if v is not None:
@@ -18,14 +18,15 @@ def len_handle_none(v):
     else:
         return 0
 
-def timetable_generator(agents: List[EnvAgent], distance_map: DistanceMap, 
-                            agents_hints: dict, np_random: RandomState = None) -> Timetable:
+
+def timetable_generator(agents: List[EnvAgent], distance_map: DistanceMap,
+                        agents_hints: dict, np_random: RandomState = None) -> Timetable:
     """
     Calculates earliest departure and latest arrival times for the agents
     This is the new addition in Flatland 3
     Also calculates the max episodes steps based on the density of the timetable
 
-    inputs: 
+    inputs:
         agents - List of all the agents rail_env.agents
         distance_map - Distance map of positions to tagets of each agent in each direction
         agent_hints - Uses the number of cities
@@ -43,35 +44,35 @@ def timetable_generator(agents: List[EnvAgent], distance_map: DistanceMap,
     timedelay_factor = 4
     alpha = 2
     max_episode_steps = int(timedelay_factor * alpha * \
-        (distance_map.rail.width + distance_map.rail.height + (len(agents) / num_cities)))
-    
+                            (distance_map.rail.width + distance_map.rail.height + (len(agents) / num_cities)))
+
     # Multipliers
     old_max_episode_steps_multiplier = 3.0
     new_max_episode_steps_multiplier = 1.5
-    travel_buffer_multiplier = 1.3 # must be strictly lesser than new_max_episode_steps_multiplier
+    travel_buffer_multiplier = 1.3  # must be strictly lesser than new_max_episode_steps_multiplier
     assert new_max_episode_steps_multiplier > travel_buffer_multiplier
     end_buffer_multiplier = 0.05
     mean_shortest_path_multiplier = 0.2
-    
+
     shortest_paths = get_shortest_paths(distance_map)
-    shortest_paths_lengths = [len_handle_none(v) for k,v in shortest_paths.items()]
+    shortest_paths_lengths = [len_handle_none(v) for k, v in shortest_paths.items()]
 
     # Find mean_shortest_path_time
     agent_speeds = [agent.speed_counter.speed for agent in agents]
-    agent_shortest_path_times = np.array(shortest_paths_lengths)/ np.array(agent_speeds)
+    agent_shortest_path_times = np.array(shortest_paths_lengths) / np.array(agent_speeds)
     mean_shortest_path_time = np.mean(agent_shortest_path_times)
 
     # Deciding on a suitable max_episode_steps
     longest_speed_normalized_time = np.max(agent_shortest_path_times)
     mean_path_delay = mean_shortest_path_time * mean_shortest_path_multiplier
     max_episode_steps_new = int(np.ceil(longest_speed_normalized_time * new_max_episode_steps_multiplier) + mean_path_delay)
-    
+
     max_episode_steps_old = int(max_episode_steps * old_max_episode_steps_multiplier)
 
     max_episode_steps = min(max_episode_steps_new, max_episode_steps_old)
-    
+
     end_buffer = int(max_episode_steps * end_buffer_multiplier)
-    latest_arrival_max = max_episode_steps-end_buffer
+    latest_arrival_max = max_episode_steps - end_buffer
 
     # Useless unless needed by returning
     earliest_departures = []
@@ -80,12 +81,12 @@ def timetable_generator(agents: List[EnvAgent], distance_map: DistanceMap,
     for agent in agents:
         agent_shortest_path_time = agent_shortest_path_times[agent.handle]
         agent_travel_time_max = int(np.ceil((agent_shortest_path_time * travel_buffer_multiplier) + mean_path_delay))
-        
+
         departure_window_max = max(latest_arrival_max - agent_travel_time_max, 1)
-        
+
         earliest_departure = np_random.randint(0, departure_window_max)
         latest_arrival = earliest_departure + agent_travel_time_max
-        
+
         earliest_departures.append(earliest_departure)
         latest_arrivals.append(latest_arrival)
 
@@ -93,15 +94,42 @@ def timetable_generator(agents: List[EnvAgent], distance_map: DistanceMap,
         agent.latest_arrival = latest_arrival
 
     return Timetable(earliest_departures=earliest_departures, latest_arrivals=latest_arrivals,
-                    max_episode_steps=max_episode_steps)
+                     max_episode_steps=max_episode_steps)
 
 
-def ttgen_flatland2(agents: List[EnvAgent], distance_map: DistanceMap, 
-                            agents_hints: dict, np_random: RandomState = None) -> Timetable:
+def ttgen_flatland2(agents: List[EnvAgent], distance_map: DistanceMap,
+                    agents_hints: dict, np_random: RandomState = None) -> Timetable:
     nMaxSteps = 1000
     return Timetable(
-        earliest_departures=[0]*len(agents),
-        latest_arrivals=[nMaxSteps]*len(agents), 
+        earliest_departures=[0] * len(agents),
+        latest_arrivals=[nMaxSteps] * len(agents),
         max_episode_steps=1000)
 
 
+def timetable_from_file(filename: Path, load_from_package: bool = None) -> timetable_generator:
+    """
+    Loads timetable from env pickle file.
+
+    Parameters
+    ----------
+    filename : Pickle file generated by RailEnvPersister.save()
+
+    Returns
+    -------
+    Tuple[List[Tuple[int,int]], List[Tuple[int,int]], List[Tuple[int,int]], List[float]]
+        initial positions, directions, targets speeds
+    """
+
+    def generator(agents: List[EnvAgent], distance_map: DistanceMap, agents_hints: dict, np_random: RandomState = None) -> Timetable:
+        env_dict = persistence.RailEnvPersister.load_env_dict(filename, load_from_package=load_from_package)
+        agents = env_dict["agents"]
+
+        max_episode_steps = env_dict.get("max_episode_steps", 0)
+        if max_episode_steps == 0:
+            warnings.warn("This env file has no max_episode_steps (deprecated) - setting to 100")
+            max_episode_steps = 100
+        earliest_departures = [a.earliest_departure for a in agents]
+        latest_arrivals = [a.latest_arrival for a in agents]
+        return Timetable(earliest_departures=earliest_departures, latest_arrivals=latest_arrivals, max_episode_steps=max_episode_steps)
+
+    return generator
