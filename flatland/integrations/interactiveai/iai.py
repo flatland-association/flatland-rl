7bf7c5f81743db204d9a37459af2ab4795d1008c
chenkins <chenkins44@gmail.com>
2025-03-07 16:23:40 +0100

    Use context api.

--- flatland/integrations/interactiveai/iai.py
+++ flatland/integrations/interactiveai/iai.py
@@ -3,19 +3,17 @@ import logging
 
 import requests
 
-from flatland.integrations.interactiveai.event_api import ApiClient
+from flatland.integrations.interactiveai.context_api import ContextApiApi, ContextIn
 from flatland.integrations.interactiveai.event_api import EventApiApi
 from flatland.integrations.interactiveai.event_api import EventIn
+from flatland.integrations.interactiveai.historic_api import HistoricApiApi
 
 logger = logging.getLogger(__name__)
 
 
 def main(
-    # api_url="http://localhost:5100/api/v1/usecases",
-    # api_url="http://localhost:5100/api/v1/context",
-    api_url="http://localhost:5001/api/v1/events",
     client_id="opfab-client",
-    username="publisher_test",
+    username="railway_user",
     password="test",
     token_url="http://frontend/auth/token"
 ):
@@ -27,18 +25,21 @@ def main(
         "Content-Type": "application/json",
     }
 
-    print(requests.get(api_url, headers=headers).json())
-    ApiClient.get_default().set_default_header("Authorization", f"Bearer {access_token}")
     events_api = EventApiApi()
+    events_api.api_client.set_default_header("Authorization", f"Bearer {access_token}")
+    context_api = ContextApiApi()
+    context_api.api_client.set_default_header("Authorization", f"Bearer {access_token}")
+    historic_api = HistoricApiApi()
+    historic_api.api_client.set_default_header("Authorization", f"Bearer {access_token}")
 
-    events = events_api.api_v1_events_get()
-    print(events)
-
+    print(events_api.api_v1_events_get())
+    print(historic_api.api_v1_traces_get())
+    print(context_api.api_v1_contexts_get())
     events_api.api_v1_events_post_with_http_info(EventIn.from_dict({
         "criticality": "LOW",
         "description": "Wonderland",
         # TODO problem with optionals in generated client code, leaving empty leads to 500
-        "end_date": datetime.datetime.now(),
+        "end_date": datetime.datetime.now() + datetime.timedelta(hours=1),
         "start_date": datetime.datetime.now(),
         "title": "Alice",
         "use_case": "Railway",
@@ -49,9 +50,25 @@ def main(
             "id_train": "20"
         }
     }))
+    context_api.api_v1_contexts_post(ContextIn.from_dict({
+        "use_case": "Railway",
+        "data": {
+            # https://opendata.swiss/de/dataset/haltestelle-perronoberflache1
+            "trains": [{
+                'failure': False, 'id_train': 'Olten	Mittelperron	8/9', 'latitude': '47.3534027132627', 'longitude': '7.90817796008907',
+                'nb_passengers_connection': 13, 'nb_passengers_onboard': 200, 'speed': 300},
+                {'failure': False, 'id_train': 'Olten	Mittelperron	2/3 ', 'latitude': '47.3515361449108', 'longitude': '7.90724203700411',
+                 'nb_passengers_connection': 13, 'nb_passengers_onboard': 200, 'speed': 300},
+                {'failure': True, 'id_train': 'Olten Hammer	Mittelperron	2/3 ', 'latitude': '47.3481023003965', 'longitude': '7.89839041783048',
+                 'nb_passengers_connection': 13, 'nb_passengers_onboard': 200, 'speed': 300}
+            ]
+        }
+    }))
 
-    events = events_api.api_v1_events_get()
-    print(events)
+    print("***")
+    print(events_api.api_v1_events_get())
+    print(historic_api.api_v1_traces_get())
+    print(context_api.api_v1_contexts_get())
 
 
 def get_access_token_password_grant(token_url, client_id, username, password) -> str:
