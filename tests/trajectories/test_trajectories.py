731828c28fec23b2ddcf8ff7c1a529df40f12f1c
chenkins <chenkins44@gmail.com>
2025-03-10 22:49:52 +0100

    Add test persistence w/ and w/o reset.

--- tests/trajectories/test_trajectories.py
+++ tests/trajectories/test_trajectories.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 import re
 import tempfile
 from pathlib import Path
@@ -8,6 +9,7 @@ import pandas as pd
 import pytest
 
 from flatland.env_generation.env_generator import env_generator
+from flatland.envs.persistence import RailEnvPersister
 from flatland.trajectories.trajectories import Policy, Trajectory, DISCRETE_ACTION_FNAME, TRAINS_ARRIVED_FNAME, TRAINS_POSITIONS_FNAME, SERIALISED_STATE_SUBDIR, \
     generate_trajectory_from_policy, evaluate_trajectory, generate_trajectories_from_metadata
 from flatland.utils.seeding import np_random, random_state_to_hashablestate
@@ -106,3 +108,45 @@ def test_gen_trajectories_from_metadata():
                 df = pd.read_csv(tmpdir / v["test_id"] / v["env_id"] / TRAINS_ARRIVED_FNAME, sep="\t")
                 assert df["success_rate"].to_list() == [sr]
                 assert df["env_time"].to_list() == [t]
+
+
+@pytest.mark.parametrize(
+    'seed',
+    [43, 44, 1001, 249385789, 289435789]
+)
+def test_persistence_reset(seed):
+    rail_env, _, _ = env_generator(seed=seed, x_dim=40, y_dim=57,)
+    np_random_generated = random_state_to_hashablestate(rail_env.np_random)
+    dict_generated = RailEnvPersister.get_full_state(rail_env)
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        RailEnvPersister.save(rail_env, os.path.join(tmpdirname, "env.pkl"))
+        reloaded, _ = RailEnvPersister.load_new(os.path.join(tmpdirname, "env.pkl"))
+    np_random_reloaded = random_state_to_hashablestate(reloaded.np_random)
+    dict_reloaded = RailEnvPersister.get_full_state(reloaded)
+
+    assert np_random_generated == np_random_reloaded
+    assert dict_generated == dict_reloaded
+
+    rail_env.reset(regenerate_rail=True, regenerate_schedule=True, random_seed=seed)
+    np_random_reset = random_state_to_hashablestate(rail_env.np_random)
+    dict_reset = RailEnvPersister.get_full_state(rail_env)
+
+    assert np_random_generated == np_random_reset
+    assert dict_generated == dict_reset
+
+    # CAVEAT: if we pass the seed but do not regenerate, this results in a different state!
+    rail_env.reset(regenerate_rail=False, regenerate_schedule=False, random_seed=seed)
+    np_random_reset_no_regenerate_same_seed = random_state_to_hashablestate(rail_env.np_random)
+    dict_reset_no_regenerate_same_seed = RailEnvPersister.get_full_state(rail_env)
+
+    assert np_random_generated != np_random_reset_no_regenerate_same_seed
+    assert dict_generated != dict_reset_no_regenerate_same_seed
+
+    # however, if we do not regenerate and pass no seed, this results in the same state.
+    rail_env.reset(regenerate_rail=False, regenerate_schedule=False)
+    np_random_reset_no_regenerate_no_seed = random_state_to_hashablestate(rail_env.np_random)
+    dict_reset_no_regenerate_no_seed = RailEnvPersister.get_full_state(rail_env)
+
+    assert np_random_generated != np_random_reset_no_regenerate_no_seed
+    assert dict_generated != dict_reset_no_regenerate_no_seed
