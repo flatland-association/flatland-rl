d378620138279a0038f62cd5acf6dbfa9a72533a
chenkins <chenkins44@gmail.com>
2025-03-14 17:24:14 +0100

    Fix test wrappers.

--- tests/ml/pettingzoo/test_wrappers.py
+++ tests/ml/pettingzoo/test_wrappers.py
@@ -1,22 +1,22 @@
 import pytest
 
+from flatland.env_generation.env_generator import env_generator
 from flatland.envs.predictions import ShortestPathPredictorForRailEnv
 from flatland.envs.rail_env_action import RailEnvActions
-from flatland.envs.rail_env_utils import env_generator
 from flatland.ml.observations.flatten_tree_observation_for_rail_env import FlattenedNormalizedTreeObsForRailEnv
 from flatland.ml.pettingzoo.wrappers import PettingzooFlatland
 
 
 def test_parallel_pettingzoo_non_compatible_observation_builder():
-    env = env_generator()
+    env, _, _ = env_generator()
     with pytest.raises(AssertionError) as exc:
         PettingzooFlatland(env).parallel_env()
     assert str(exc.value) == "<class 'flatland.envs.observations.TreeObsForRailEnv'> is not gym-compatible, missing get_observation_space"
 
 
 def test_parallel_pettingzoo():
-    env = PettingzooFlatland(env_generator(
-        obs_builder_object=FlattenedNormalizedTreeObsForRailEnv(max_depth=3, predictor=ShortestPathPredictorForRailEnv(max_depth=50)))).parallel_env()
+    raw_env, _, _ = env_generator(obs_builder_object=FlattenedNormalizedTreeObsForRailEnv(max_depth=3, predictor=ShortestPathPredictorForRailEnv(max_depth=50)))
+    env = PettingzooFlatland(raw_env).parallel_env()
     observations, infos = env.reset()
     assert len(observations) == 7
     assert len(infos) == 7
