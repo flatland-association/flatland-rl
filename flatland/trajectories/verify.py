4e658bb0ad04fc840d10a4d88aabecc1da1fb7f2
chenkins <chenkins44@gmail.com>
2025-03-15 17:28:07 +0100

    Add temporary fix for agents going from MALFUNCTION_OFF_MAP to READY_TO_DEPART instead of STOPPED if cell is already occupied.

--- flatland/trajectories/verify.py
+++ flatland/trajectories/verify.py
@@ -8,13 +8,12 @@ import tqdm
 from flatland.envs.persistence import RailEnvPersister
 from flatland.envs.rail_env import RailEnv
 from flatland.trajectories.trajectories import Trajectory
-
 # TODO cleanup heuristic baseline
 from src.observation.full_state_observation import FullStateObservationBuilder
 from src.policy.deadlock_avoidance_policy import DeadLockAvoidancePolicy
 
-if __name__ == '__main__':
 
+def verify():
     for p in tqdm.tqdm(sorted(Path("../../episodes/malfunction_deadlock_avoidance_heuristics/").rglob("**/TrainMovementEvents.trains_positions.tsv"))):
 
         df = pd.read_csv(str(p), delimiter="\t")
@@ -30,27 +29,33 @@ if __name__ == '__main__':
         if len(non_exclusives) > 0:
             warnings.warn(str(p))
             print(non_exclusives)
+
             ep_id = f"{test}_{level}"
 
-            env_path = (p.parent.parent / "serialised_state" / f"{test}_{level}.pkl").resolve()
+            try_fix(p.parent.parent, ep_id)
 
-            data_dir = Path("/tmp/blup")
-            data_dir.mkdir(exist_ok=True)
 
-            policy_pkg = ""
-            policy_cls = "DeadLockAvoidancePolicy"
+def try_fix(data_dir, ep_id):
+    data_dir = (data_dir / "serialised_state" / f"{ep_id}.pkl").resolve()
+    output_dir = Path("/tmp/blup")
+    output_dir.mkdir(exist_ok=True)
+    policy_pkg = ""
+    policy_cls = "DeadLockAvoidancePolicy"
+    env, _ = RailEnvPersister.load_new(str(data_dir))
+    env: RailEnv = env
+    env.obs_builder = FullStateObservationBuilder()
+    # TODO improve
+    env.obs_builder.env = env
+    Trajectory.create_from_policy(
+        policy=DeadLockAvoidancePolicy(),
+        data_dir=output_dir,
+        env=env
+    )
 
-            env, _ = RailEnvPersister.load_new(str(env_path))
-            env: RailEnv = env
-            env.obs_builder = FullStateObservationBuilder()
-            # TODO improve
-            env.obs_builder.env = env
 
-            Trajectory.create_from_policy(
-                policy=DeadLockAvoidancePolicy(),
-                data_dir=data_dir,
-                env=env
-            )
+if __name__ == '__main__':
+    # verify()
+
 
     # 52%|█████▎    | 21/40 [00:00<00:00, 21.42it/s]/Users/che/workspaces/flatland-rl-2/flatland/trajectories/verify.py:19: UserWarning: ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_02/Level_1/event_logs/TrainMovementEvents.trains_positions.tsv
     # warnings.warn(str(p))
@@ -74,13 +79,21 @@ if __name__ == '__main__':
     # warnings.warn(str(p))
     # 100%|██████████| 40/40 [00:06<00:00,  5.96it/s]
 
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_02/Level_1/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_0/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_1/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_2/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_3/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_4/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_6/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_7/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_8/event_logs/TrainMovementEvents.trains_positions.tsv
-    # ../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_9/event_logs/TrainMovementEvents.trains_positions.tsv
+
+    failed = [
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_02/Level_1", "Test_02_Level_1"),
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_0", "Test_03_Level_0"),
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_1", "Test_03_Level_1"),
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_2", "Test_03_Level_2"),
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_3", "Test_03_Level_3"),
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_4", "Test_03_Level_4"),
+        ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_6", "Test_03_Level_6"),
+        # ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_7", "Test_03_Level_7"),
+        # ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_8", "Test_03_Level_8"),
+        # ("../../episodes/malfunction_deadlock_avoidance_heuristics/Test_03/Level_9", "Test_03_Level_9"),
+    ]
+    for data_dir, ep_id in failed:
+        try_fix(
+            data_dir=Path(data_dir).resolve(),
+            ep_id=ep_id
+        )
