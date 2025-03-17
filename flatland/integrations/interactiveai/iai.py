00302095248be5afcfa067c435aed67a5591b30a
chenkins <chenkins44@gmail.com>
2025-03-17 18:46:44 +0100

    Run olten scenario from main

--- flatland/integrations/interactiveai/iai.py
+++ flatland/integrations/interactiveai/iai.py
@@ -1,7 +1,9 @@
 import datetime
 import logging
-import os
+import pickle
 import time
+import warnings
+from pathlib import Path
 from typing import Optional
 
 import requests
@@ -19,10 +21,16 @@ logger = logging.getLogger(__name__)
 
 
 class FlatlandInteractiveAI(FlatlandCallbacks):
-    def __init__(self, client_id="opfab-client",
+    def __init__(self,
+                 coordinate_map,
+                 client_id="opfab-client",
                  username="admin",
                  password="test",
-                 token_url="http://frontend/auth/token"):
+                 token_url="http://frontend/auth/token",
+                 step_to_millis=1000,
+                 collect_only=False,
+                 now=None
+                 ):
         self.token_url = token_url
         self.client_id = client_id
         self.username = username
@@ -32,6 +40,14 @@ class FlatlandInteractiveAI(FlatlandCallbacks):
         self.historic_api: HistoricApiApi = None
         self.events_api: EventApiApi = None
         self.in_malfunction = {}
+        self.collect_only = collect_only
+        self.step_to_millis = step_to_millis
+        self.events = []
+        self.contexts = []
+        self.coordinate_map = coordinate_map
+        self.now = now
+        if self.now is None:
+            self.now = datetime.datetime.now()
 
     def connect(self):
         access_token = self._get_access_token_password_grant(token_url=self.token_url, client_id=self.client_id, username=self.username, password=self.password)
@@ -56,10 +72,10 @@ class FlatlandInteractiveAI(FlatlandCallbacks):
         return json_response.get("access_token")
 
     def on_episode_step(
-            self,
-            *,
-            env: Optional[RailEnv] = None,
-            **kwargs,
+        self,
+        *,
+        env: Optional[RailEnv] = None,
+        **kwargs,
     ) -> None:
         """Called on each episode step (after the action(s) has/have been logged).
 
@@ -71,9 +87,9 @@ class FlatlandInteractiveAI(FlatlandCallbacks):
         also after the results of this step (observation, reward, terminated, truncated,
         infos) have been logged to the given `episode` object.
         """
-        if self.access_token is None:
+        if self.access_token is None and not self.collect_only:
             self.connect()
-        if env._elapsed_steps == 1:
+        if env._elapsed_steps == 1 and not self.collect_only:
             # self.events_api.api_v1_delete_
             # self.historic_api.api_v1_delete_all_data_delete()
             # self.context_api.api_v1_delete_all_data_delete()
@@ -82,62 +98,64 @@ class FlatlandInteractiveAI(FlatlandCallbacks):
             print(self.events_api.api_v1_events_get())
             print(self.historic_api.api_v1_traces_get())
             print(self.context_api.api_v1_contexts_get())
-        now = datetime.datetime.now()
-
-        # TODO refine
-        step_to_millis = 1000
-        # TODO mapping
-        # https://opendata.swiss/de/dataset/haltestelle-perronoberflache1
-        origin_lat = 47.3534027132627
-        origin_lon = 7.90817796008907
-        xy_delta = 0.001
 
         for agent in env.agents:
             prev = self.in_malfunction.get(agent.handle, False)
             cur = agent.malfunction_handler.in_malfunction
             self.in_malfunction[agent.handle] = cur
             if not prev and cur:
-                self.events_api.api_v1_events_post(EventIn.from_dict({
+                event = {
                     "use_case": "Railway",
                     "criticality": "LOW",
                     "title": f"Malfunction Train {agent.handle}",
                     "description": f"Malfunction Train {agent.handle}",
-                    "start_date": now,
-                    # TODO offset?
-                    "end_date": now + datetime.timedelta(milliseconds=agent.malfunction_handler.malfunction_down_counter * step_to_millis),
+                    "start_date": self.now,
+                    "end_date": self.now + datetime.timedelta(milliseconds=agent.malfunction_handler.malfunction_down_counter * self.step_to_millis),
                     "data": {
                         "event_type": "Malfunction",
                         "delay": 300,
                         "agent_id": f"{agent.handle}",
                         "id_train": f"{agent.handle}",
                     }
-                }))
+                }
+                self.events.append(event)
+                if not self.collect_only:
+                    self.events_api.api_v1_events_post(EventIn.from_dict(event))
+
         # TODO use non-blocking calls or queue?
-        self.context_api.api_v1_contexts_post(ContextIn.from_dict({
+        for agent in env.agents:
+            if agent.position is not None and agent.position not in self.coordinate_map:
+                warnings.warn(f"Missing mapping for {agent.position}")
+        context = {
             "use_case": "Railway",
             "data": {
                 "trains": [
                     {
                         'id_train': f'Train {agent.handle}',
-                        'latitude': f"{origin_lat + agent.position[1] * xy_delta}",
-                        'longitude': f'{origin_lon + agent.position[0] * xy_delta}',
-                    } for agent in env.agents if agent.position is not None]
+                        'latitude': f"{self.coordinate_map[agent.position][0]}",
+                        'longitude': f'{self.coordinate_map[agent.position][1]}',
+                    } for agent in env.agents if agent.position is not None
+                                                 # TODO
+                                                 and agent.position in self.coordinate_map]
             }
-        }))
-        # TODO refine
-        time.sleep(step_to_millis * 0.001)
+        }
+        self.contexts.append(context)
+        if not self.collect_only:
+            self.context_api.api_v1_contexts_post(ContextIn.from_dict(context))
+
+        time.sleep(self.step_to_millis * 0.001)
 
 
 if __name__ == '__main__':
-    data_sub_dir = "malfunction_deadlock_avoidance_heuristics/Test_03/Level_2"
-    ep_id = "Test_03_Level_2"
-    _dir = os.getenv("BENCHMARK_EPISODES_FOLDER")
-    assert _dir is not None, _dir
-    assert os.path.exists(_dir), _dir
-    data_dir = os.path.join(_dir, data_sub_dir)
+    # data from https://github.com/flatland-association/flatland-scenarios/tree/scenario-olten/scenario_olten/data
+    data_dir = Path("../../../../flatland-scenarios2/scenario_olten/data").resolve()
+    trajectory = Trajectory(data_dir, ep_id="olten")
 
-    TrajectoryEvaluator(Trajectory(data_dir=data_dir, ep_id=ep_id), callbacks=FlatlandInteractiveAI()).evaluate()
-    # TrajectoryEvaluator(Trajectory(data_dir=data_dir, ep_id=ep_id)).evaluate() # 29.38it/s
-    # fiai = FlatlandInteractiveAI()
-    # fiai.connect()
-    # fiai.on_episode_step()
+    with (data_dir / "position_to_latlon_olten.pkl").open("rb") as file_in:
+        position_to_latlon_olten = pickle.load(file_in)
+    cb = FlatlandInteractiveAI(position_to_latlon_olten, collect_only=True)
+
+    TrajectoryEvaluator(trajectory, cb).evaluate()
+
+    print(cb.contexts)
+    print(cb.events)
