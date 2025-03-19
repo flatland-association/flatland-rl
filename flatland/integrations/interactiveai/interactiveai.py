d983c6aa3f56fb82ed667c555ce4808e659d7871
chenkins <chenkins44@gmail.com>
2025-03-19 16:18:44 +0100

    Update documentation.
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- flatland/integrations/interactiveai/interactiveai.py
+++ flatland/integrations/interactiveai/interactiveai.py
@@ -1,3 +1,13 @@
+"""
+FlatlandCallbacks for InteractiveAI (https://github.com/AI4REALNET/InteractiveAI).
+
+The callbacks create context and events during a scenario run.
+If an InteractiveAI instance is up and running, the callbacks  send out HTTP POST requests to InteractiveAI contexts and events REST API endpoints.
+In this notebook, we just log the contexts and events that would be sent out.
+
+- The agent positions are sent as context, with geo-coordinates for display on a map.
+- Agent malfunctions are sent as events.
+"""
 import datetime
 import logging
 import time
