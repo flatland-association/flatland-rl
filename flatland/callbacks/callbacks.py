b1801f87ae2dde45e18bd91954b44212d3a968d5
Christian Eichenberger <chenkins44@gmail.com>
2025-03-21 16:05:03 +0100

    InteractiveAI Integration. (#152)
    * Explore API.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Add IAI stubs from openapi*.json
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Re-import openapi with railway data schemas.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Use context api.
    
    * Implement Flatland callbacks.
    
    * Run scenario and post to InteractiveAI.
    
    * Fix script.
    
    * Allow create_from_policy to get env directly, skipping env generation.
    
    * Run olten scenario from main
    
    * Extract GenerateMovieCallbacks.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Fix tests.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Fix tests.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Add notebook illustrating FlatlandInteractiveAICallbacks.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Fix test.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Cleanup notebook.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Fix make_multi_callbacks.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Add end step in evaluator.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Add documentation.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Update documentation.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Run interactiveai notebook in ci.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Install ffmpeg for notebooks.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    * Add sudo.
    
    Signed-off-by: chenkins <chenkins44@gmail.com>
    
    ---------
    
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- flatland/callbacks/callbacks.py
+++ flatland/callbacks/callbacks.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from pathlib import Path
+from typing import Optional, List
 
 from flatland.core.env import Environment
 
@@ -12,10 +13,33 @@ class FlatlandCallbacks:
     By default, all of these callbacks are no-ops.
     """
 
+    def on_episode_start(
+        self,
+        *,
+        env: Optional[Environment] = None,
+        data_dir: Path = None,
+        **kwargs,
+    ) -> None:
+        """Callback run right after an Episode has been started.
+
+        This method gets called after `env.reset()`.
+
+        Parameters
+        ---------
+            env : Environment
+                the env
+            data_dir : Path
+                trajectory data dir
+            kwargs:
+                Forward compatibility placeholder.
+        """
+        pass
+
     def on_episode_step(
         self,
         *,
         env: Optional[Environment] = None,
+        data_dir: Path = None,
         **kwargs,
     ) -> None:
         """Called on each episode step (after the action(s) has/have been logged).
@@ -27,5 +51,59 @@ class FlatlandCallbacks:
         The exact time of the call of this callback is after `env.step([action])` and
         also after the results of this step (observation, reward, terminated, truncated,
         infos) have been logged to the given `episode` object.
+
+        Parameters
+        ---------
+            env : Environment
+                the env
+            data_dir : Path
+                trajectory data dir
+            kwargs:
+                Forward compatibility placeholder.
         """
         pass
+
+    def on_episode_end(
+        self,
+        *,
+        env: Optional[Environment] = None,
+        data_dir: Path = None,
+        **kwargs,
+    ) -> None:
+        """Called when an episode is done (after terminated/truncated have been logged).
+
+        The exact time of the call of this callback is after `env.step([action])`
+
+        Parameters
+        ---------
+            env : Environment
+                the env
+            data_dir : Path
+                trajectory data dir
+            kwargs:
+                Forward compatibility placeholder.
+        """
+        pass
+
+
+# https://github.com/ray-project/ray/blob/3b94e5ff0038798a6955cde37459a0d30aa718c4/rllib/callbacks/utils.py#L41
+def make_multi_callbacks(*_callback_list: FlatlandCallbacks):
+    class _MultiFlatlandCallbacks(FlatlandCallbacks):
+        IS_CALLBACK_CONTAINER = True
+
+        def __init__(self, callback_list: List[FlatlandCallbacks]):
+            self._callback_list = callback_list
+
+        def on_episode_start(self, **kwargs) -> None:
+            for callback in self._callback_list:
+                callback.on_episode_start(**kwargs)
+
+        def on_episode_step(self, **kwargs) -> None:
+            for callback in self._callback_list:
+                callback.on_episode_step(**kwargs)
+
+        def on_episode_end(self, **kwargs) -> None:
+            for callback in self._callback_list:
+                callback.on_episode_end(**kwargs)
+
+    return _MultiFlatlandCallbacks(_callback_list)
