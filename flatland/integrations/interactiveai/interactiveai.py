caf73fe0f71f35918dc3efb99ab1bf9faddac190
chenkins <chenkins44@gmail.com>
2025-03-26 11:41:46 +0100

    Add configuration option for non-default URL for InteractiveAI events, context and historic API.
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- flatland/integrations/interactiveai/interactiveai.py
+++ flatland/integrations/interactiveai/interactiveai.py
@@ -10,6 +10,7 @@ In this notebook, we just log the contexts and events that would be sent out.
 """
 import datetime
 import logging
+import pickle
 import time
 import warnings
 from pathlib import Path
@@ -17,12 +18,15 @@ from typing import Optional, Dict, Tuple
 
 import requests
 
+from evaluators.trajectory_evaluator import TrajectoryEvaluator
 from flatland.callbacks.callbacks import FlatlandCallbacks
 from flatland.envs.rail_env import RailEnv
+from flatland.integrations.interactiveai import event_api, context_api, historic_api
 from flatland.integrations.interactiveai.context_api import ContextApiApi, ContextIn
 from flatland.integrations.interactiveai.event_api import EventApiApi
 from flatland.integrations.interactiveai.event_api import EventIn
 from flatland.integrations.interactiveai.historic_api import HistoricApiApi
+from trajectories.trajectories import Trajectory
 
 logger = logging.getLogger(__name__)
 
@@ -34,9 +38,12 @@ class FlatlandInteractiveAICallbacks(FlatlandCallbacks):
                  username: str = "admin",
                  password: str = "test",
                  token_url: str = "http://frontend/auth/token",
+                 event_api_host="http://localhost:5001",
+                 context_api_host="http://localhost:5100",
+                 history_api_host="http://localhost:5200",
                  step_to_millis: int = 1000,
                  collect_only: bool = False,
-                 now: datetime.datetime = None
+                 now: datetime.datetime = None,
                  ):
         """
 
@@ -52,6 +59,13 @@ class FlatlandInteractiveAICallbacks(FlatlandCallbacks):
             InteractiveAI password
         token_url : str
             InteractiveAI token_url
+         event_api_host: str
+            Defaults to http://localhost:5001",
+         context_api_host : str
+            Defaults to "http://localhost:5100",
+         history_api_host : str
+            Defaults to "http://localhost:5200",
+
         step_to_millis : int
             how many millis delay till next `env.step()`
         collect_only : bool
@@ -65,6 +79,9 @@ class FlatlandInteractiveAICallbacks(FlatlandCallbacks):
         self.username = username
         self.password = password
         self.access_token = None
+        self.event_api_host = event_api_host
+        self.context_api_host = context_api_host
+        self.history_api_host = history_api_host
         self.context_api: ContextApiApi = None
         self.historic_api: HistoricApiApi = None
         self.events_api: EventApiApi = None
@@ -82,11 +99,13 @@ class FlatlandInteractiveAICallbacks(FlatlandCallbacks):
         access_token = self._get_access_token_password_grant(token_url=self.token_url, client_id=self.client_id, username=self.username, password=self.password)
         print(access_token)
 
-        self.events_api = EventApiApi()
+        self.events_api = EventApiApi(event_api.ApiClient(event_api.Configuration(host=self.event_api_host)))
         self.events_api.api_client.set_default_header("Authorization", f"Bearer {access_token}")
-        self.context_api = ContextApiApi()
+
+        self.context_api = ContextApiApi(context_api.ApiClient(context_api.Configuration(host=self.context_api_host)))
         self.context_api.api_client.set_default_header("Authorization", f"Bearer {access_token}")
-        self.historic_api = HistoricApiApi()
+
+        self.historic_api = HistoricApiApi(historic_api.ApiClient(historic_api.Configuration(host=self.history_api_host)))
         self.historic_api.api_client.set_default_header("Authorization", f"Bearer {access_token}")
         self.access_token = access_token
 
@@ -119,15 +138,6 @@ class FlatlandInteractiveAICallbacks(FlatlandCallbacks):
         """
         if self.access_token is None and not self.collect_only:
             self.connect()
-        if env._elapsed_steps == 1 and not self.collect_only:
-            # self.events_api.api_v1_delete_
-            # self.historic_api.api_v1_delete_all_data_delete()
-            # self.context_api.api_v1_delete_all_data_delete()
-            for event in self.events_api.api_v1_events_get():
-                self.events_api.api_v1_event_event_id_delete(event_id=event.id_event)
-            print(self.events_api.api_v1_events_get())
-            print(self.historic_api.api_v1_traces_get())
-            print(self.context_api.api_v1_contexts_get())
 
         for agent in env.agents:
             prev = self.in_malfunction.get(agent.handle, False)
@@ -174,3 +184,27 @@ class FlatlandInteractiveAICallbacks(FlatlandCallbacks):
             self.context_api.api_v1_contexts_post(ContextIn.from_dict(context))
 
         time.sleep(self.step_to_millis * 0.001)
+
+
+if __name__ == '__main__':
+    # scenario Olten has step every 3 seconds for an hour
+    STEPS_ONE_HOUR = 1300
+    # how many ms per step if replaying in real-time
+    REALTIME_STEP_TO_MILLIS = 3600 / STEPS_ONE_HOUR * 1000
+    # run faster...
+    SPEEDUP = 1000
+
+    # https://github.com/flatland-association/flatland-scenarios/tree/main/scenario_olten/data
+    # TODO data is based on env < 4.0.6, need to re-generate
+    data_dir = Path("../flatland-scenarios/scenario_olten/data")
+
+    with (data_dir / "position_to_latlon_olten.pkl").resolve().open("rb") as file_in:
+        position_to_latlon_olten = pickle.loads(file_in.read())
+
+    trajectory = Trajectory(data_dir=data_dir, ep_id="olten")
+
+    # see above for configuration options
+    cb = FlatlandInteractiveAICallbacks(position_to_latlon_olten, collect_only=False, step_to_millis=REALTIME_STEP_TO_MILLIS / SPEEDUP)
+    TrajectoryEvaluator(trajectory, cb).evaluate(end_step=150)
+    print(cb.events)
+    print(cb.contexts)
