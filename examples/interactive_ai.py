e6b16239a73a7f19c2212a10719e77c54a2d4d1c
chenkins <chenkins44@gmail.com>
2025-03-31 17:33:22 +0200

    Extract InteractiveAI script to examples folder.
    Signed-off-by: chenkins <chenkins44@gmail.com>

--- examples/interactive_ai.py
+++ examples/interactive_ai.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 from pathlib import Path
 
@@ -6,7 +7,6 @@ from flatland.integrations.interactiveai.interactiveai import FlatlandInteractiv
 from flatland.trajectories.trajectories import Trajectory
 
 if __name__ == '__main__':
-    # https://github.com/flatland-association/flatland-scenarios/raw/refs/heads/scenario-olten-fix/scenario_olten/data/OLTEN_PARTIALLY_CLOSED_v1.zip
     # scenario Olten has step every 3 seconds for an hour
     STEPS_ONE_HOUR = 1300  # 1h + additional time for agents to leave the map
     # how many ms per step if replaying in real-time
@@ -14,10 +14,12 @@ if __name__ == '__main__':
     # run faster... limiting factor becomes environment stepping time and blocking requests InteractiveAI platform
     SPEEDUP = 1000
 
-    scenario = "olten"
+    scenario = "olten_partially_closed"
 
-    # https://github.com/flatland-association/flatland-scenarios/tree/main/scenario_olten/data
-    data_dir = Path(f"../../flatland-scenarios/scenario_olten/data/{scenario}")
+    # https://github.com/flatland-association/flatland-scenarios/raw/refs/heads/scenario-olten-fix/scenario_olten/data/OLTEN_PARTIALLY_CLOSED_v1.zip
+
+    _dir = os.getenv("SCENARIOS_FOLDER", "../scenarios")
+    data_dir = Path(f"{_dir}/scenario_olten/data/{scenario}")
 
     with (data_dir / "position_to_latlon.pkl").resolve().open("rb") as file_in:
         position_to_latlon_olten = pickle.loads(file_in.read())
@@ -26,6 +28,6 @@ if __name__ == '__main__':
 
     # see above for configuration options, use collect_only=False for live POSTing
     cb = FlatlandInteractiveAICallbacks(position_to_latlon_olten, collect_only=True, step_to_millis=REALTIME_STEP_TO_MILLIS / SPEEDUP)
-    TrajectoryEvaluator(trajectory, cb).evaluate()  # end_step=150)
+    TrajectoryEvaluator(trajectory, cb).evaluate(end_step=150)
     print(cb.events)
     print(cb.contexts)
