9b3f7d47eb00a5422549bb3dd193580537cb7dfd
chenkins <chenkins44@gmail.com>
2025-06-20 12:04:09 +0200

    feat: implement malfunction generator as effects generator.

---
+++ flatland/envs/malfunction_effects_generators.py
@@ -0,0 +1,13 @@
+from flatland.core.effects_generator import EffectsGenerator
+
+
+class MalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
+
+    def __init__(self, malfunction_generator):
+        super().__init__()
+        self.malfunction_generator = malfunction_generator
+
+    def on_episode_step_start(self, env: "RailEnv", *args, **kwargs) -> "RailEnv":
+        for agent in env.agents:
+            agent.malfunction_handler.generate_malfunction(self.malfunction_generator, env.np_random)
+        return env
