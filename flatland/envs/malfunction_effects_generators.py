58131a963e7cfc1bc8ad8cf894fbb688465257a5
chenkins <chenkins44@gmail.com>
2025-08-25 17:26:37 +0200

    ci: dummy.

--- flatland/envs/malfunction_effects_generators.py
+++ flatland/envs/malfunction_effects_generators.py
@@ -11,3 +11,5 @@ class MalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
         for agent in env.agents:
             agent.malfunction_handler.generate_malfunction(self.malfunction_generator, env.np_random)
         return env
+
+# dummy
