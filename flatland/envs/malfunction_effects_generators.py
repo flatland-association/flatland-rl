ca6751e228bf2ea6038b55e640f1da4e66ac0254
chenkins <chenkins44@gmail.com>
2025-09-23 16:30:16 +0200

    feat(malfunction effects generator): add option to pass condition by package and class name. Add on_map_state_condition.

--- flatland/envs/malfunction_effects_generators.py
+++ flatland/envs/malfunction_effects_generators.py
@@ -1,8 +1,10 @@
+import importlib
 from typing import Callable, List
 
 from flatland.core.effects_generator import EffectsGenerator
 from flatland.core.grid.grid_utils import IntVector2D
 from flatland.envs import malfunction_generators as mal_gen
+from flatland.envs.agent_utils import EnvAgent
 from flatland.envs.step_utils.states import TrainState
 
 
@@ -21,6 +23,10 @@ class MalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
 MalfunctionCondition = Callable[["EnvAgent", int], bool]
 
 
+def on_map_state_condition(env_agent: EnvAgent, elapsed_steps: int) -> bool:
+    return env_agent.state_machine.state.is_on_map_state()
+
+
 class ConditionalMalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
     def __init__(self,
                  malfunction_rate: float = None,
@@ -29,6 +35,8 @@ class ConditionalMalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
                  earliest_malfunction: int = None,
                  max_num_malfunctions: int = None,
                  condition: MalfunctionCondition = None,
+                 condition_pkg: str = None,
+                 condition_cls: str = None,
                  ):
         """
         Generate agent malfunctions conditionally with conditional rate and duration.
@@ -47,6 +55,10 @@ class ConditionalMalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
             Defaults to `None`.
         condition : MalfunctionCondition
             Additional condition. Defaults to None.
+        condition_pkg : str
+            Additional condition to be created instead of instance via `condition`. Defaults to None.
+        condition_cls : str
+            Additional condition to be created instead of instance via `condition`. Defaults to None.
         """
         super().__init__()
 
@@ -61,6 +73,9 @@ class ConditionalMalfunctionEffectsGenerator(EffectsGenerator["RailEnv"]):
         self._max_num_malfunctions = int(max_num_malfunctions) if max_num_malfunctions is not None else None
         self._num_malfunctions = 0
         self._condition = condition
+        if condition_pkg is not None and condition_cls is not None:
+            module = importlib.import_module(condition_pkg)
+            self._condition = getattr(module, condition_cls)
 
     def on_episode_step_start(self, env: "RailEnv", *args, **kwargs) -> "RailEnv":
         if self._earliest_condition is not None and env._elapsed_steps < self._earliest_condition:
