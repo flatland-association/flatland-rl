ab64b5de487e02477e3327717e4021cbd80ddcc0
chenkins <chenkins44@gmail.com>
2025-10-05 15:51:03 +0200

    feat(tree obs): add UngroupedFlattenedTreeObsForRailEnv.

--- flatland/ml/observations/flatten_tree_observation_for_rail_env.py
+++ flatland/ml/observations/flatten_tree_observation_for_rail_env.py
@@ -85,11 +85,12 @@ class FlattenedTreeObsForRailEnv(GymObservationBuilder[RailEnv, np.ndarray], Tre
 
         return data, distance, agent_data
 
-    def split_tree_into_feature_groups(self, tree, max_tree_depth: int) -> (np.ndarray, np.ndarray, np.ndarray):
+    def split_tree_into_feature_groups(self, root_node: Node, max_tree_depth: int) -> (np.ndarray, np.ndarray, np.ndarray):
         """
+
         This function splits the tree into three difference arrays of values:
 
-        - `data` (5 features per node):
+        - `data` (6 features per node):
             - `node.dist_own_target_encountered`
             - `node.dist_other_target_encountered`
             - `node.dist_other_agent_encountered`
@@ -112,10 +113,10 @@ class FlattenedTreeObsForRailEnv(GymObservationBuilder[RailEnv, np.ndarray], Tre
         `( data ('N', 'L', 'F', 'R', 'B', ...), distance ('N', 'L', 'F', 'R', 'B', ...), agent_data( 'N', 'L', 'F', 'R', 'B', ...))`
         See `get_len_flattened()` for the length of the flattened structure.
         """
-        data, distance, agent_data = self._split_node_into_feature_groups(tree)
+        data, distance, agent_data = self._split_node_into_feature_groups(root_node)
 
         for direction in TreeObsForRailEnv.tree_explored_actions_char:
-            sub_data, sub_distance, sub_agent_data = self._split_subtree_into_feature_groups(tree.childs[direction], 1,
+            sub_data, sub_distance, sub_agent_data = self._split_subtree_into_feature_groups(root_node.childs[direction], 1,
                                                                                              max_tree_depth)
             data = np.concatenate((data, sub_data))
             distance = np.concatenate((distance, sub_distance))
@@ -244,3 +245,102 @@ class FlattenedNormalizedTreeObsForRailEnv(FlattenedTreeObsForRailEnv):
     def get(self, handle: Optional[AgentHandle] = 0) -> np.ndarray:
         observation = super(FlattenedNormalizedTreeObsForRailEnv, self).get(handle)
         return self.normalize_obs(observation)
+
+
+class UngroupedFlattenedTreeObsForRailEnv(GymObservationBuilder[RailEnv, np.ndarray], TreeObsForRailEnv):
+    """
+    Gym-ified and flattened normalized tree observation without feature grouping.
+    """
+
+    NUM_FEATURES = 12
+    NUM_BRANCHES = 4
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def _get_len_data(tree_depth: int, num_features):
+        k = num_features
+        for _ in range(tree_depth):
+            k = k * UngroupedFlattenedTreeObsForRailEnv.NUM_BRANCHES + num_features
+        return k
+
+    def _traverse_subtree(self, node, current_tree_depth: int, max_tree_depth: int) -> np.ndarray:
+        if node == -np.inf:
+            remaining_depth = max_tree_depth - current_tree_depth
+            # reference: https://stackoverflow.com/questions/515214/total-number-of-nodes-in-a-tree-data-structure
+            num_remaining_nodes = int((4 ** (remaining_depth + 1) - 1) / (4 - 1))
+            return [-np.inf] * num_remaining_nodes * self.NUM_FEATURES
+
+        data = list(node[:UngroupedFlattenedTreeObsForRailEnv.NUM_FEATURES])
+
+        if not node.childs:
+            return data
+
+        for direction in TreeObsForRailEnv.tree_explored_actions_char:
+            sub_data = self._traverse_subtree(node.childs[direction], current_tree_depth + 1, max_tree_depth)
+            data = np.concatenate((data, sub_data))
+
+        return data
+
+    def traverse_tree(self, root_node: Node, max_tree_depth: int) -> np.ndarray:
+        """
+        This function extracts:
+
+        - `data` (12 features per node):
+            - `node.dist_own_target_encountered`
+            - `node.dist_other_target_encountered`
+            - `node.dist_other_agent_encountered`
+            - `node.dist_potential_conflict`
+            - `node.dist_unusable_switch`
+            - `node.dist_to_next_branch`
+            - `node.dist_min_to_target`
+            - `node.num_agents_same_direction`
+            - `node.num_agents_opposite_direction`
+            - `node.num_agents_malfunctioning`
+            - `node.speed_min_fractional`
+            - `node.num_agents_ready_to_depart`
+
+        Subtrees are traversed depth-first in pre-order (i.e. Node 'N' itself, 'L', 'F', 'R', 'B').
+        See `get_len_flattened()` for the length of the flattened structure.
+        """
+        data = list(root_node[:12])
+
+        for direction in UngroupedFlattenedTreeObsForRailEnv.tree_explored_actions_char:
+            sub_data = self._traverse_subtree(root_node.childs[direction], 1, max_tree_depth)
+            data = np.concatenate((data, sub_data))
+
+        return data
+
+    def get(self, handle: Optional[AgentHandle] = 0) -> np.ndarray:
+        root_node = super(UngroupedFlattenedTreeObsForRailEnv, self).get(handle)
+        return self.traverse_tree(root_node, self.max_depth)
+
+    def get_observation_space(self, handle: int = 0) -> gym.Space:
+        k = self.get_len_flattened()
+        return gym.spaces.Box(low=-np.inf, high=np.inf, shape=(k,), dtype=np.float64)
+
+    def get_len_flattened(self):
+        """
+        The total size `S[k]` of the flattened structure for `max_tree_depth=k` is recursively defined by:
+        - `S[0] = NUM_FEATURES`
+        - `S[k+1] = S[k] * NUM_BRANCHES + NUM_FEATURES`
+        for
+        - `NUM_FEATURES=12`
+        - `NUM_BRANCHES=4`
+
+        I.e.
+        - max_depth=1 -> 60
+        - max_depth=2 -> 252
+        - max_depth=3 -> 1020
+        - ...
+
+        Returns
+        -------
+        Length of the flattened tree obs.
+        """
+
+        k = UngroupedFlattenedTreeObsForRailEnv.NUM_FEATURES
+        for _ in range(self.max_depth):
+            k = k * UngroupedFlattenedTreeObsForRailEnv.NUM_BRANCHES + UngroupedFlattenedTreeObsForRailEnv.NUM_FEATURES
+        return k
