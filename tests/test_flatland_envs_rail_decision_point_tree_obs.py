e995578e41c8de26dc64d1113b73f76f8d247601
chenkins <chenkins44@gmail.com>
2025-10-07 15:29:35 +0200

    feat(graph tree obs): graph tree obs returning preliminary node structure.

--- tests/test_flatland_envs_rail_decision_point_tree_obs.py
+++ tests/test_flatland_envs_rail_decision_point_tree_obs.py
@@ -1,3 +1,4 @@
+from flatland.envs.observations import Node
 from flatland.envs.rail_decision_point_tree_obs import DecisionPointTreeObs
 from flatland.envs.rail_env import RailEnv
 from flatland.envs.rail_env_action import RailEnvActions
@@ -10,12 +11,23 @@ def test_decisions_point_tree_obs():
 
     env = RailEnv(width=rail_map.shape[1], height=rail_map.shape[0], rail_generator=rail_from_grid_transition_map(rail, optionals),
                   number_of_agents=1,
-                  obs_builder_object=DecisionPointTreeObs(depth=2),
+                  obs_builder_object=DecisionPointTreeObs(max_depth=2),
                   random_seed=56)
     env.reset()
-    assert env.agents[0].initial_position == (6, 6)
+    env.agents[0].initial_position = (5, 6)
     # a bit hacky: initial position at dead-end needs to have correct initial direction
     env.agents[0].initial_direction = 0
 
     obs, _, _, _ = env.step({0: RailEnvActions.MOVE_FORWARD})
     obs, _, _, _ = env.step({0: RailEnvActions.MOVE_FORWARD})
+    # depth 0
+    assert isinstance(obs[0], Node)
+    # depth 1
+    for i in range(2):
+        assert isinstance(obs[0].childs[i], Node)
+        for j in range(2):
+            # depth 2
+            assert isinstance(obs[0].childs[i].childs[j], Node)
+            for k in range(2):
+                # depth 3
+                assert k not in obs[0].childs[i].childs[j].childs
