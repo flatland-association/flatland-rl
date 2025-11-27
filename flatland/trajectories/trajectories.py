9a928ef10c548addb108fcd543076e5899c47498
chenkins <chenkins44@gmail.com>
2025-11-27 11:48:08 +0100

    perf(Trajectory Evaluator): Pandas lookups are very slow, add caching.

--- flatland/trajectories/trajectories.py
+++ flatland/trajectories/trajectories.py
@@ -1,6 +1,7 @@
 import ast
 import os
 import uuid
+from collections import defaultdict
 from pathlib import Path
 from typing import Optional, Tuple, Any, Dict
 
@@ -221,6 +222,18 @@ class Trajectory:
         self._trains_rewards_dones_infos_collect.append({
             'episode_id': self.ep_id, 'env_time': env_time, 'agent_id': agent_id, 'reward': reward, 'info': info, 'done': done
         })
+
+    def build_cache(self):
+        self.action_cache = defaultdict(lambda: defaultdict(dict))
+        for item in self.actions[self.actions["episode_id"] == self.ep_id].to_records():
+            self.action_cache[item["env_time"]][item["agent_id"]] = RailEnvActions.from_value(item["action"])
+        self.position_cache = defaultdict(lambda: defaultdict(dict))
+        for item in self.trains_positions[self.trains_positions["episode_id"] == self.ep_id].to_records():
+            p, d = item['position']
+            self.position_cache[item["env_time"]][item["agent_id"]] = (p, d)
+        self.trains_rewards_dones_infos_cache = defaultdict(lambda: defaultdict(dict))
+        for data in self.trains_rewards_dones_infos[self.trains_rewards_dones_infos["episode_id"] == self.ep_id].to_records():
+            self.trains_rewards_dones_infos_cache[data["env_time"]][data["agent_id"]] = (data["reward"], data["done"], data["info"])
 
     def position_lookup(self, env_time: int, agent_id: int) -> Tuple[Tuple[int, int], int]:
         """Method used to retrieve the stored position (if available).
