23a405c432a3e97a29c33abc1580fb2894b8b76e
chenkins <chenkins44@gmail.com>
2026-01-21 16:19:00 +0100

    chore: cleanup.

--- flatland/trajectories/policy_grid_runner.py
+++ flatland/trajectories/policy_grid_runner.py
@@ -163,14 +163,4 @@ def generate_trajectories_from_metadata(
             assert exc.code == 0
 
 
-if __name__ == '__main__':
-    metadata_csv = Path("./episodes/trajectories/malfunction_deadlock_avoidance_heuristics/metadata.csv").resolve()
-    data_dir = Path("./episodes/trajectories/malfunction_deadlock_avoidance_heuristics").resolve()
-    generate_trajectories_from_metadata([
-        "--metadata-csv", metadata_csv,
-        "--data-dir", data_dir,
-        "--policy-pkg", "flatland_baselines.deadlock_avoidance_heuristic.policy.deadlock_avoidance_policy",
-        "--policy-cls", "DeadLockAvoidancePolicy",
-        "--obs-builder-pkg", "flatland_baselines.deadlock_avoidance_heuristic.observation.full_env_observation",
-        "--obs-builder-cls", "FullEnvObservation"
-    ])
+
